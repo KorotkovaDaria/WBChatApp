cacca193136b7c51c4b1a8e119f85a85a6a591f7
Korotkova Daria <Dkuvashova@mail.ru>
2024-07-10 10:40:14 +0300

    Add WalkthroughView

--- WBChatAppSUI/View/WalkthroughView.swift
+++ WBChatAppSUI/View/WalkthroughView.swift
@@ -9,9 +9,58 @@ import SwiftUI
 
 struct WalkthroughView: View {
     
+    @State var isPresented = false
+    
     var body: some View {
-        VStack {
-            
+        NavigationStack {
+            VStack {
+                Spacer()
+                Image(Resources.ImageTitle.DefaultImage.Illustration)
+                    .resizable()
+                    .frame(width: 262, height: 271)
+                    .padding(42)
+                Text(Resources.DefaultStrings.walkViewString1)
+                    .textStyle(with: .neading2)
+                    .foregroundColor(Resources.Colors.NeutralColor.active)
+                Spacer()
+                Spacer()
+                
+                VStack {
+                    Text(Resources.DefaultStrings.walkViewString2)
+                        .textStyle(with: .metadata2)
+                        .foregroundColor(Resources.Colors.NeutralColor.weakSecondaryText)
+                        .padding(1)
+                    
+                    HStack(spacing: 3) {
+                        Button(Resources.DefaultStrings.walkViewString3) {
+                            //do something
+                        }
+                        .textStyle(with: .metadata2)
+                        .foregroundColor(Resources.Colors.BrandColor.bcDefaultButton)
+                        
+                        Text(Resources.DefaultStrings.walkViewString4)
+                            .textStyle(with: .metadata2)
+                            .foregroundColor(Resources.Colors.NeutralColor.weakSecondaryText)
+                        
+                        Button(Resources.DefaultStrings.walkViewString5) {
+                            //do something
+                        }
+                        .textStyle(with: .metadata2)
+                        .foregroundColor(Resources.Colors.BrandColor.bcDefaultButton)
+                    }
+                    .padding(.bottom, 12)
+                    
+                    Button(Resources.DefaultStrings.walkViewString6) {
+                        isPresented.toggle()
+                    }
+                    .buttonStyle(StatefulButtonStyle(size: CGSize(width: 327, height: 52), state: .fillNormal))
+                }
+                Spacer()
+                    .navigationDestination(isPresented: $isPresented) {
+                        VerificationView()
+                    }
+            }
+            .multilineTextAlignment(.center)
         }
     }
 }
