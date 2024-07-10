04c63bfe991d4cc68c41d8b1bf539d9316650821
Korotkova Daria <Dkuvashova@mail.ru>
2024-07-10 11:19:04 +0300

    Add HW4

--- WBChatAppSUI/View/ContentView_TabView.swift
+++ WBChatAppSUI/View/ContentView_TabView.swift
@@ -9,31 +9,17 @@ import SwiftUI
 
 import SwiftUI
 
-enum Tabs: Hashable {
-    case contacts
-    case chats
-    case more
-}
-
-final class Router: ObservableObject {
-    @Published var selectedTab: Tabs = .contacts
-}
-
 struct ContentView_TabView: View {
-    @ObservedObject var router: Router = .init()
-
+    //@ObservedObject var router: Router = .init()
+    @StateObject var router = Router.shared
+    
     var body: some View {
         TabView(selection: $router.selectedTab) {
-            ContactsScreen().tabItem {Label("Контакты", systemImage: "person.2")}.tag(Tabs.contacts)
+            ContactsScreen().tabItem { Label("Контакты", systemImage: Resources.ImageTitle.SystemImage.tabBarPerson) }.tag(Tabs.contacts)
             ChatsScreen().tabItem { Label("Чаты", systemImage: Resources.ImageTitle.SystemImage.tabBarChats) }.tag(Tabs.chats)
             MoreScreen().tabItem { Label("Еще", systemImage: Resources.ImageTitle.SystemImage.tabBarMore) }.tag(Tabs.more)
-                }
-            }
         }
-
-struct ContentView_TabView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView_TabView()
     }
 }
+
 
